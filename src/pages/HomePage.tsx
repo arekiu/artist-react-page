fe9a2971d37121248fd0185602b8677f910219d4
Alex <alexis.mar.schmidt@gmail.com>
2023-08-10 14:47:56 +0200

    add animation in homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,16 @@
+import animation from "../assets/animation.gif";
+
 function HomePage(): JSX.Element {
   return (
     <div className="container">
-      <h1 className="titles">Home</h1>
+      <h1 className="titles">Welcome</h1>
+
+      <div
+        className="card border-dark mb-3 col-md-2 col-sm-6 col-10"
+        id="profile-img"
+      >
+        <img className="img-artist" src={animation} alt="Johnny draws" />
+      </div>
     </div>
   );
 }
