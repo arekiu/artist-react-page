905c10d6c94b36ac82e4c5e4e99c9e837d2c1768
Alex <alexis.mar.schmidt@gmail.com>
2023-08-11 14:14:30 +0200

    update icons in gallery

--- src/pages/GalleryPage.tsx
+++ src/pages/GalleryPage.tsx
@@ -3,6 +3,7 @@ import iconDigital from "../assets/icon-digital.png";
 import iconPen from "../assets/icon-pen.png";
 import iconPencil from "../assets/icon-pencil.png";
 import iconPirograf from "../assets/icon-pirograf.png";
+import iconAll from "../assets/icon-all.png";
 
 function GalleryPage(): JSX.Element {
   return (
@@ -10,29 +11,33 @@ function GalleryPage(): JSX.Element {
       <h1 className="titles">Gallery</h1>
 
       <div className="icons text-center">
-        <div className="single-icon text-center col-md-1 col-sm-2 col-3">
+        <div className="single-icon text-center col-md-1 col-sm-2 col-2">
+          <img className="img-artist" src={iconAll} alt="all icon" />
+          <h5>All</h5>
+        </div>
+        <div className="single-icon text-center col-md-1 col-sm-2 col-2">
           <img className="img-artist" src={iconBrush} alt="Brush icon" />
           <h5>Paint</h5>
         </div>
-        <div className="single-icon text-center col-md-1 col-sm-2 col-3">
+        <div className="single-icon text-center col-md-1 col-sm-2 col-2">
           <img className="img-artist" src={iconDigital} alt="Digital icon" />
           <h5>Digital</h5>
         </div>
-        <div className="single-icon text-center col-md-1 col-sm-2 col-3">
+        <div className="single-icon text-center col-md-1 col-sm-2 col-2">
           <img className="img-artist" src={iconPen} alt="Pen icon" />
           <h5>Pen</h5>
         </div>
-        <div className="single-icon text-center col-md-1 col-sm-2 col-3">
+        <div className="single-icon text-center col-md-1 col-sm-2 col-2">
           <img className="img-artist" src={iconPencil} alt="Pencil icon" />
           <h5>Pencil</h5>
         </div>
-        <div className="single-icon text-center col-md-1 col-sm-2 col-3">
+        <div className="single-icon text-center col-md-1 col-sm-2 col-2">
           <img
             className="img-artist"
             src={iconPirograf}
             alt="Pirography icon"
           />
-          <h5>Pirography</h5>
+          <h5>Fire</h5>
         </div>
       </div>
     </div>
